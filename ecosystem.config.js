5fdb2b8fca421ad3cfa5afc2bb318e8674e4472f
NMCONLINE <nmconline@nmc-lan-biz.ts.nevermindcoffee.cn>
2026-07-05 22:05:02 -0700

    chore: 更新依赖及 ecosystem 配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,5 +16,21 @@ module.exports = {
         TZ: 'Asia/Shanghai',
       },
     },
+    {
+      name: 'pm-bot',
+      script: '/Users/nmconline/apps/pm/.venv/bin/python',
+      args: ['-m', 'wecom.bot'],
+      cwd: '/Users/nmconline/apps/pm',
+      interpreter: 'none',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        OBJC_DISABLE_INITIALIZE_FORK_SAFETY: 'YES',
+        TZ: 'Asia/Shanghai',
+        WECHAT_BOT_ID: 'aibzQIGj08lrFN-Qp9vUaAAL1OrJU_bTlsI',
+        WECHAT_BOT_SECRET: 'G6T7Q3RFJ4tzEFe2IFBbBr7tIr2amQdi5QqtyGagZPo'
+      },
+    },
   ],
 };
